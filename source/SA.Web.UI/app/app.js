4ce88a098e75121f4c2f658a70e5181b0b53911b
jigneshnarain <jignesh.narain@gmail.com>
2015-04-12 12:18:48 +0200

    Base Project Commit

---
+++ source/SA.Web.UI/app/app.js
@@ -0,0 +1,15 @@
+﻿var app = angular.module('surveyapp', ['ngRoute', 'ngResource']);
+
+app.config(function ($routeProvider, $locationProvider) {
+    $locationProvider.html5Mode({enabled: true, requireBase: false});
+    $routeProvider
+    .when("/viewname", {
+        controller: "controller",
+        templateUrl: "path to html page"
+    })
+     .otherwise({ redirectTo: "/" });
+});
+
+app.constant('ngSettings', {
+    apiBaseUri: 'http://localhost:46781/api/'
+});
---
+++ source/SP.Api/Bootstrapper.cs
@@ -0,0 +1,19 @@
+﻿using Autofac;
+using Autofac.Integration.WebApi;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SA.Api
+{
+    internal sealed class Bootstrapper
+    {
+        internal static IContainer Run()
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterApiControllers();
+            return builder.Build();
+        }
+    }
+}
