c80decd53869d64af8f12f285e39019109cf0659
Jignesh Narain <jignesh.narain@gmail.com>
2015-05-23 11:07:36 +0200

    Replace select query with a stored procedure to retreive user details. Configured claims for authenticated users.

--- source/SA.Authentication.Repository/UserRepository.cs
+++ source/SA.Authentication.Repository/UserRepository.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Dapper;
+using System.Data;
 namespace SA.Authentication.Repository
 {
     public class UserRepository : IUserRepository
@@ -19,7 +20,7 @@ namespace SA.Authentication.Repository
         {
             using (var db = accessor())
             {
-               return db.ExecuteQuery<Models.User>("SurvayDataConnection", query => query.Query<Models.User>(new CommandDefinition("select * from system.[user] where username=@username", new { username = username })).FirstOrDefault());
+                return db.ExecuteQuery<Models.User>("SurvayDataConnection", query => query.Query<Models.User>(new CommandDefinition("[System].[GetUser]", new { Username = username }, commandType: CommandType.StoredProcedure)).FirstOrDefault());
             }
         }
     }
