50ec1955a171ff16d6b66dfb0f938cdeabf7f166
Jignesh Narain <jignesh.narain@gmail.com>
2015-05-24 18:35:55 +0200

    fxied checkbox

--- source/SA.Web.UI/app/app.js
+++ source/SA.Web.UI/app/app.js
@@ -1,4 +1,4 @@
-﻿var app = angular.module('surveyapp', ['ngRoute', 'ngResource']);
+﻿var app = angular.module('surveyapp', ['ngRoute', 'ngResource','checklist-model']);
 
 app.config(function ($routeProvider, $locationProvider) {
     $locationProvider.html5Mode({enabled: true, requireBase: false});
